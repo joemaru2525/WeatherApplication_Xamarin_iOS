e8fee0b672f3c39d3a8af99469e1daec9474a44d
joemaru2525 <joe.maru.60195@gmail.com>
2018-02-10 18:11:03 +0900

    画像以外のスクレイピング完成

--- YahooWeather_iOS/ViewController.cs
+++ YahooWeather_iOS/ViewController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Drawing;
+using Foundation;
+using HtmlAgilityPack;
 using UIKit;
 
 namespace YahooWeather_iOS
@@ -26,6 +28,69 @@ namespace YahooWeather_iOS
             this.labelPrecip03.Text = "50%";
             this.labelPrecip04.Text = "100%";
 
+            //(1)指定したサイトのHTMLをストリームで取得する
+            const string url = "https://weather.yahoo.co.jp/weather/jp/14/4610.html";
+            var urlstring = string.Format(url);
+
+            //(2)指定したサイトのHTMLをストリームで取得する
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            using (var client = new System.Net.WebClient())
+            {
+                var html = client.DownloadString(new Uri(urlstring));
+
+                // HtmlAgilityPack.HtmlDocumentオブジェクトにHTMLを読み込ませる
+                doc.LoadHtml(html);
+            }
+
+            //(3)XPathを使って情報を抽出
+            //タイトル
+            HtmlNodeCollection node0 =
+            doc.DocumentNode.SelectNodes("//title");
+            this.labelTitle.Text = node0[0].InnerText;
+
+            //Anounce Date & Time（発表日時）
+            HtmlNodeCollection node1 =
+            doc.DocumentNode.SelectNodes("//div[@class='yjw_title_h2 yjw_clr']//p[@class='yjSt yjw_note_h2']");
+            this.labelAnnounce.Text = node1[0].InnerText;    //node1(0).InnerText
+
+            //WeatherDate（対象日）
+            HtmlNodeCollection node2 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//p[@class='date']");
+            this.labelDate.Text = node2[0].InnerText;
+
+            //Weather（天候）
+            HtmlNodeCollection node3 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//p[@class='pict']");
+            this.labelWeather.Text = node3[0].InnerText;
+
+            //High Temp（最高気温）
+            HtmlNodeCollection node4 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//ul[@class='temp']//li[@class='high']");
+            this.labelTempHigh.Text = "最高気温 [前日差]： " + node4[0].InnerText;
+
+            //Low Temp（最低気温）
+            HtmlNodeCollection node5 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//ul[@class='temp']//li[@class='low']");
+            this.labelTempLow.Text = "最低気温 [前日差]： " + node5[0].InnerText;
+
+            //Precip1[0-6]（降水確率）
+            HtmlNodeCollection node6 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//tr[@class='precip']//td");
+            this.labelPrecip01.Text = node6[0].InnerText;
+
+            //Precip1[6-12]（降水確率）
+            this.labelPrecip02.Text = node6[1].InnerText;
+
+            //Precip1[12-18]（降水確率）
+            this.labelPrecip03.Text = node6[2].InnerText;
+
+            //Precip1[18-24]（降水確率）
+            this.labelPrecip04.Text = node6[3].InnerText;
+
+            //WeatherPicture（天気画像）
+            HtmlNodeCollection node7 =
+            doc.DocumentNode.SelectNodes("//div[@class='forecastCity']//p[@class='pict']//img");
+            //this.imageWeather.Image = node7[0].GetAttributeValue("src", "");
        
         }
 
